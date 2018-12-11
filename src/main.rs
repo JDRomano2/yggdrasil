319df1f2cfdd2d8ce24c4d068a0662008d52de34
Joseph Romano <jdromano2@gmail.com>
2018-12-11 12:00:42 -0500

    Create ontology metadata struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+struct Ontology {
+  name: String,
+  version: String,
+  author: String,
+}
+
 fn main() {
     println!("Hello, world!");
     println!("");
@@ -5,4 +11,15 @@ fn main() {
     println!("written in the Rust programming language.");
     println!("");
     println!("This is an early effort, so stay tuned for planned features.");
+
+    let ont = Ontology {
+        name: String::from("VenomKB"),
+        version: String::from("2.0.0a"),
+        author: String::from("Joseph D. Romano"),
+    };
+
+    println!("");
+    println!("Ontology name: {}", ont.name);
+    println!("Ontology version: {}", ont.version);
+    println!("Ontology author: {}", ont.author);
 }
