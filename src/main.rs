34fa5687b1faa5ecb85ecafe79b23c6476b8bf6c
Joseph Romano <JDRomano2@gmail.com>
2018-12-11 11:37:48 -0500

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    println!("Hello, world!");
+    println!("");
+    println!("This is a new project for interacting with bioontologies,");
+    println!("written in the Rust programming language.");
+    println!("");
+    println!("This is an early effort, so stay tuned for planned features.");
+}
